c73539ebb7b21644d40b12c65c8c62df832cd519
Ivan <telitsinivan05@yandex.ru>
2023-03-03 01:55:41 +0300

    modified:   login.js 	modified:   manifest.json

--- login.js
+++ login.js
@@ -1,8 +1,14 @@
 chrome.storage.local.get(["enable", "login", "password"], result => {
-    document.getElementById("id_login").value = result.login !== undefined ? result.login : ''
-    document.getElementById("id_password").value = result.password !== undefined ? result.password : ''
-    if (document.getElementsByClassName("errorlist")[0] === undefined
-        && result.enable && result.password !== '' && result.password !== '') {
-        document.getElementById("mirea-users-loginform").submit()
+    if (window.location.href === "https://online-edu.mirea.ru/" && document.getElementById("login") !== undefined) {
+        document.getElementById("login_username").value = result.login !== undefined ? result.login : ''
+        document.getElementById("login_password").value = result.password !== undefined ? result.password : ''
+        document.getElementById("login").submit()
+    } else {
+        document.getElementById("id_login").value = result.login !== undefined ? result.login : ''
+        document.getElementById("id_password").value = result.password !== undefined ? result.password : ''
+        if (document.getElementsByClassName("errorlist")[0] === undefined
+            && result.enable && result.password !== '' && result.password !== '') {
+            document.getElementById("mirea-users-loginform").submit()
+        }
     }
 })
